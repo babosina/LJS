d5e4aea798f837486fef137449d955d77206b8df
babosina <babos77@gmail.com>
2017-05-31 11:44:14 +0300

    ChessBoard

--- EloquentJS/ch2Ex3.js
+++ EloquentJS/ch2Ex3.js
@@ -1,3 +1,15 @@
 // Chess board
 
+/*chessBoard = '# # # # \n # # # #\n# # # # \n # # # #\n# # # # \n # # # #\n# # # # \n # # # #';
 
+console.log(chessBoard);*/
+
+size = 8;
+var count = 1;
+for (count; count <= size; count++) {
+    if (count % 2 === 0) {
+        console.log(' # # # #');
+    } else {
+        console.log('# # # # ');
+    }
+}
